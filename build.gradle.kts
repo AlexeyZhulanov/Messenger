1e06bfa4a91632211968bd65bbab7791a98d06ec
alexey <Leha.Zhulanov.03@mail.ru>
2026-02-14 15:53:02 +0500

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-    dependencies {
-        classpath(libs.gradle)
-        classpath(libs.kotlin.gradle.plugin)
-        classpath(libs.com.google.devtools.ksp.gradle.plugin)
-        classpath(libs.hilt.android.gradle.plugin)
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.google.services) apply false
 }
